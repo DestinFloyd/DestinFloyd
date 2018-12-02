6a29afecd0cbbfe518e1393b64edfb01c4de19c1
destin floyd <destin.b.floyd@gmail.com>
2018-12-02 00:12:07 -0500

    designed page 4 Projects

--- app.js
+++ app.js
@@ -2,18 +2,39 @@
 // console.log("working");
 
 // making a function to spin the dive around then change the page
-var project = document.getElementById("projects");
+//  must change the position to realtive and z-inde to 3 when theyre animated so theyre on TOPPPPPP; (2 items have z -index 2 in project so why its 3 needed)
+
 
 function moveAround1(){
-var x = 0;
+
+
 var project = document.getElementById("projects");
 
 
-for(let i = 0; i <= 75; i++){
-    x ++;
-    project.style.marginLeft =  x + "%";
 
+    var xRight = 25;
+    var xLeft = 0;
+    var yUp = 0;
+    var yDown = 0;
+
+project.style.zIndex = 4;
+
+
+for(let i = 0; i <= 10; i++){
+    xRight ++;
+    yDown ++;
+    project.style.marginLeft =  xRight + "%";
+    project.style.marginTop =  yDown + "%";
+    project.style.marginRight = xLeft + "%";
+    project.style.marginBottom =  yUp + "%";
 }
 
 
+
+
+
+
 }
+// else{
+//     clearInterval(all);
+// }
