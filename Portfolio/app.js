e33ec9a95728fa8261aedcbe2bd8668f2187766d
destin floyd <destin.b.floyd@gmail.com>
2019-03-11 14:00:18 -0400

    styling, changing background photo

---
+++ Portfolio/app.js
@@ -0,0 +1,18 @@
+function closeInfo(){
+  document.getElementById("contactHideaway").style.visibility = "hidden"
+  document.getElementById("linkedinLink").style.visibility = "hidden"
+}
+
+function openInfo(){
+  document.getElementById("contactHideaway").style.color = "visible"
+  document.getElementById("linkedinLink").style.color = "visible"
+}
+
+function closeAbout(){
+  document.getElementById("aboutHideaway").style.color = "hidden"
+}
+
+function openAbout(){
+  document.getElementById("aboutHideaway").style.color = "visible"
+}
+
